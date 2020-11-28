72bedbd238eda79d77a670514d29b58f2150bb9b
Mohammad Arif Shariati <shariati@mohammads-mbp.local>
2020-11-29 00:19:27 +0300

    integrated with graphql and listing all users in contactsscreen

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,15 +1,42 @@
-import * as React from 'react';
+import React,{ useState, useEffect} from 'react';
 import { StyleSheet, FlatList } from 'react-native';
 
 import { View } from '../components/Themed';
 
+import { API, graphqlOperation } from 'aws-amplify'
+import { listUsers } from '../graphql/queries'
 //components
 import ContactListItem from '../components/ContactListItem';
 
 //mock Data
-import users from '../data/Users';
+//import users from '../data/Users';
 
 export default function ContactsScreen() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(()=>{
+
+    const fetchUsers = async () =>{
+
+      try{
+
+        const userData = await API.graphql(graphqlOperation(listUsers));
+
+        if(userData){
+          setUsers(userData.data.listUsers.items);
+        }
+
+      }
+      catch(error){
+        console.log(error.message);
+      }
+      
+    }
+
+    fetchUsers();
+  },[]);
+
   return (
     <View style={styles.container}>
       <FlatList 
